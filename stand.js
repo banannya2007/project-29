feaa9d903243ff59d2e01439c2f9d3b33459a179
banannya2007 <73088793+banannya2007@users.noreply.github.com>
2021-01-01 16:47:54 +0800

    Add files via upload

---
+++ stand.js
@@ -0,0 +1,20 @@
+class stand{
+    constructor(x, y, width, height) {
+        var options = {
+            isStatic:true
+        }
+        this.width = width;
+        this.height = height;
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        World.add(world, this.body);
+      }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        rectMode(CENTER);
+        rect(0,0,this.width, this.height);
+        pop();
+      }
+}
